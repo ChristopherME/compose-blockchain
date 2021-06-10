a4aebd4ed634a19106341ed57964ce8f1ecf0884
Christopher Elias <christopher.mike.96@gmail.com>
2021-06-10 14:43:13 -0500

    create DefaultTestNetworkMiddleware.kt for tests utils

---
+++ app/src/test/java/com/christopherelias/blockchain/utils/DefaultTestNetworkMiddleware.kt
@@ -0,0 +1,23 @@
+package com.christopherelias.blockchain.utils
+
+import com.christopherelias.blockchain.core.network.middleware.NetworkMiddleware
+import com.christopherelias.blockchain.core.network.models.NetworkMiddlewareFailure
+
+/*
+ * Created by Christopher Elias on 10/06/2021
+ * christopher.mike.96@gmail.com
+ *
+ * Lima, Peru.
+ */
+
+class DefaultTestNetworkMiddleware(
+    private val isMiddlewareValid: Boolean,
+    private val failureMessage: String = ""
+) : NetworkMiddleware() {
+
+    override val failure: NetworkMiddlewareFailure
+        get() = NetworkMiddlewareFailure(middleWareExceptionMessage = failureMessage)
+
+    override fun isValid(): Boolean = isMiddlewareValid
+
+}
